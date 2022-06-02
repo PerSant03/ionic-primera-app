ce4cb7ce8a1145d028b6306f31ac3f3a8d138ee7
unknown <dep.sistemas.gim@gmail.com>
2022-06-02 13:38:05 -0500

    Fin sección 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
+
+  ngOnInit() {
+
+    let _Numero = 3;
+    //debugger;
+    _Numero = _Numero + 3;
+
+    _Numero = 0
+
+    console.log(_Numero);
+  }
 
 }
